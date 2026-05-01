703d2e45720264cb82048ece44a34d4309fa7eb0
akashthatti <90924467+akashthatti@users.noreply.github.com>
2026-05-01 19:38:47 +0530

    Update TestngRunner.java

--- selenium/src/test/java/TestngTestRunner/TestngRunner.java
+++ selenium/src/test/java/TestngTestRunner/TestngRunner.java
@@ -3,7 +3,7 @@ package TestngTestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "/src/test/java/feature/newFeatures.feature",glue="stepdefinitions",monochrome = true,
+@CucumberOptions(features = "./src/test/java/feature/newFeatures.feature",glue="stepdefinitions",monochrome = true,
 dryRun = false)
 public class TestngRunner extends AbstractTestNGCucumberTests {
 
